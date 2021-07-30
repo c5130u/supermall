fdf977d1ac0d1b016a5581c69eb1609af662f53e
lizi <1499968494@qq.com>
2021-07-30 09:40:03 +0800

    s

--- src/components/common/MainTabBar.vue
+++ src/components/common/MainTabBar.vue
@@ -3,46 +3,46 @@
         <tab-bar>
             <tab-bar-item path="/home" activeColor="pink">
                 <template v-slot:bar-icon>
-                <img src="~assets/img/tabbar/home.svg" alt="">
+                    <img src="~assets/img/tabbar/home.svg" alt="">
                 </template>
                 <template v-slot:bar-icon-active>
-                <img src="~assets/img/tabbar/home_active.svg" alt="">
+                    <img src="~assets/img/tabbar/home_active.svg" alt="">
                 </template>
                 <template v-slot:bar-text>
-                <p>首页</p>
+                    <p>首页</p>
                 </template>
             </tab-bar-item>
             <tab-bar-item path="/category" activeColor="pink">
                 <template v-slot:bar-icon>
-                <img src="~assets/img/tabbar/category.svg" alt="">
+                    <img src="~assets/img/tabbar/category.svg" alt="">
                 </template>
                 <template v-slot:bar-icon-active>
-                <img src="~assets/img/tabbar/category_active.svg" alt="">
+                    <img src="~assets/img/tabbar/category_active.svg" alt="">
                 </template>
                 <template v-slot:bar-text>
-                <p>分类</p>
+                    <p>分类</p>
                 </template>
             </tab-bar-item>
             <tab-bar-item path="/shopcart" activeColor="pink">
                 <template v-slot:bar-icon>
-                <img src="~assets/img/tabbar/shopcart.svg" alt="">
+                    <img src="~assets/img/tabbar/shopcart.svg" alt="">
                 </template>
                 <template v-slot:bar-icon-active>
-                <img src="~assets/img/tabbar/shopcart_active.svg" alt="">
+                    <img src="~assets/img/tabbar/shopcart_active.svg" alt="">
                 </template>
                 <template v-slot:bar-text>
-                <p>购物车</p>
+                    <p>购物车</p>
                 </template>
             </tab-bar-item>
             <tab-bar-item path="/profile" activeColor="pink">
                 <template v-slot:bar-icon>
-                <img src="~assets/img/tabbar/profile.svg" alt="">
+                    <img src="~assets/img/tabbar/profile.svg" alt="">
                 </template>
                 <template v-slot:bar-icon-active>
-                <img src="~assets/img/tabbar/profile_active.svg" alt="">
+                    <img src="~assets/img/tabbar/profile_active.svg" alt="">
                 </template>
                 <template v-slot:bar-text>
-                <p>我的</p>
+                    <p>我的</p>
                 </template>
             </tab-bar-item>
         </tab-bar>
